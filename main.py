40dc862ec721699715f8f79a9160151ad348db7a
steveabecassis <abecassis.steve@gmail.com>
2023-06-12 22:34:09 +0300

    all step on small data (without visualization)

--- main.py
+++ main.py
@@ -18,10 +18,40 @@ def load_data(file_path='./arxiv-metadata-oai-snapshot.json'):
             c += 1
             data = json.loads(line)
             relevant_data.append(data)
+            # to test the code on small data
             if c == 1000:
                 break
     # Convert the relevant_data list to a DataFrame
     return pd.DataFrame(relevant_data)
+
+
+def c_tf_idf(documents, m, ngram_range=(1, 1)):
+    count = CountVectorizer(ngram_range=ngram_range, stop_words="english").fit(documents)
+    t = count.transform(documents).toarray()
+    w = t.sum(axis=1)
+    tf = np.divide(t.T, w)
+    sum_t = t.sum(axis=0)
+    idf = np.log(np.divide(m, sum_t)).reshape(-1, 1)
+    tf_idf = np.multiply(tf, idf)
+    return tf_idf, count
+
+def extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20):
+    words = count.get_feature_names_out()
+    labels = list(docs_per_topic.Topic)
+    tf_idf_transposed = tf_idf.T
+    indices = tf_idf_transposed.argsort()[:, -n:]
+    top_n_words = {label: [(words[j], tf_idf_transposed[i][j]) for j in indices[i]][::-1] for i, label in enumerate(labels)}
+    return top_n_words
+
+
+def extract_topic_sizes(df):
+    topic_sizes = (df.groupby(['Topic'])
+                   .abstract
+                   .count()
+                   .reset_index()
+                   .rename({"Topic": "Topic", "abstract": "Size"}, axis='columns')
+                   .sort_values("Size", ascending=False))
+    return topic_sizes
 
 
 if __name__ == '__main__':
@@ -40,10 +70,28 @@ if __name__ == '__main__':
     # Cluster reduced embeddings
     hdbscan_model = HDBSCAN(min_cluster_size=15, metric='euclidean', cluster_selection_method='eom',prediction_data=True)
     hdbscan_model.fit_predict(embeddings_abstracts)
-    clusters = np.unique(hdbscan_model.labels_)
+    # clusters = np.unique(hdbscan_model.labels_,return_counts=True)
+    print(f"There is {len(np.unique(hdbscan_model.labels_))} topics")
+    df['Topic'] = hdbscan_model.labels_
 
     # Tokenize topics
     vectorizer_model = CountVectorizer(stop_words="english")
+    cv = vectorizer_model.fit_transform(df['abstract'])
+    vectorizer_model.get_feature_names_out()
+
+    # Extract topic words
+    docs_per_topic = df.groupby(['Topic'], as_index=False).agg({'abstract': ' '.join})
+    tf_idf, count = c_tf_idf(docs_per_topic.abstract.values, m=len(df))
+    top_n_words = extract_top_n_words_per_topic(tf_idf, count, docs_per_topic, n=20)
+    topic_sizes = extract_topic_sizes(df)
+
+    topic_sizes.head(10)
+
+    # Top 10 words of cluster 1
+    top_n_words[1][:10]
+
+
+    # Visualisation
 
 
 
